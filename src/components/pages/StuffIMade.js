c9ae114e1a95ff865e563cdd94661a05cd9a5eb7
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-05-22 11:25:30 -0600

    scroll responsive

--- src/components/pages/StuffIMade.js
+++ src/components/pages/StuffIMade.js
@@ -2,7 +2,7 @@ import { IoArrowForwardOutline } from 'react-icons/io5';
 
 const StuffIMade = () => {
   return (
-    <div className="work-item-continer">
+    <div className="work-item-continer stuff">
       <h1 className="things">
         Some Things I've Made <IoArrowForwardOutline />
       </h1>
