e8628c07f63cb8934402995b8e6366914a6b7f96
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-05-26 12:50:37 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,16 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer">
-        <div className="footer-input">Get in Touch:</div>
+        <div className="footer-input">
+          Get In Touch
+          <p className="footer-text">
+            I am currently open to web design and development opportunities.
+            Need a refresher on your existing website? I'm curretnly taking new
+            clients for the month of June. If you are interested in learning
+            more please don't hesitate to reach out and I will get back to you
+            within 24 hours!{' '}
+          </p>
+        </div>
         <div className="footer-icons">
           {' '}
           <AiOutlineMail />
