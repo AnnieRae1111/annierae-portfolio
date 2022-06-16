3381cdfe2c0a7749d31e9e75f96cb8d43885fc1f
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-06-16 17:53:55 -0600

    footer media query

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
 import '../css/footer.css';
-
 import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer">
-        <div className="footer-input">
+      <div className="footer container">
+        <div className="footer-input container">
           Get In Touch :
           <p className="footer-text">
             I am currently open to web design and development opportunities.
@@ -19,9 +18,32 @@ const Footer = () => {
         </div>
         <div className="footer-icons">
           {' '}
-          <AiOutlineMail />
-          <AiFillLinkedin />
-          <AiFillGithub />
+          <a
+            className="footer-link"
+            href="mailto:annirae.elizabeth@gmail.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {' '}
+            <AiOutlineMail />{' '}
+          </a>
+          <a
+            className="footer-link"
+            href="https://www.linkedin.com/in/annierae-cioccolanti/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiFillLinkedin />
+          </a>
+          <a
+            className="footer-link"
+            href="https://github.com/AnnieRae1111"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {' '}
+            <AiFillGithub />
+          </a>
         </div>
       </div>
     </footer>
