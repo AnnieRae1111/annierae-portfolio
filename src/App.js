b3f5a184f2e57650ca3929f700d76a9219ce4913
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-06-20 12:54:44 -0600

    about two page'

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import NavBar from './components/pages/Navbar';
 import NavbarResponsive from './components/pages/NavbarResponsive';
 import LandingPage from './components/pages/LandingPage';
 import About from './components/pages/About';
-import Services from './components/pages/Services';
-import Portfolio from './components/pages/Portfolio';
-import Inquire from './components/pages/Inquire';
-import Marquee from './components/Marquee';
-import Work from './components/Work';
-import WorkGoodKarma from './components/WorkGoodKarma';
-import WorkWar from './components/WorkWar';
+// import Services from './components/pages/Services';
+// import Portfolio from './components/pages/Portfolio';
+// import Inquire from './components/pages/Inquire';
+// import Marquee from './components/Marquee';
+// import Work from './components/Work';
+// import WorkGoodKarma from './components/WorkGoodKarma';
+// import WorkWar from './components/WorkWar';
 import './css/App.css';
 import ProjectScroller from './components/ProjectScroller';
 import GraphicDesign from './components/GraphicDesign';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import clouds from './assets/Clouds.mp4';
+// import clouds from './assets/Clouds.mp4';
 import AboutTwo from './components/pages/About2';
 function App() {
   return (
@@ -52,7 +51,7 @@ function App() {
               </>
             }
           ></Route>
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<AboutTwo />} />
           {/* <Route path="/services" element={<Services />} /> */}
           <Route path="/portfolio" element={<ProjectScroller />} />
           {/* <Route path="/inquire" element={<Inquire />} /> */}
--- src/components/pages/About2.js
+++ src/components/pages/About2.js
@@ -1,49 +1,58 @@
-// import '../../css/about2.css';
+import '../../css/about2.css';
 import '../../css/about.css';
+import me from '../../assets/annierae-profile-photo.jpeg';
 const AboutTwo = () => {
   return (
-    <div className="about-container">
-      <h1 className="about-header text-center container">More About Me :</h1>
-      <h4 className="about-text container">
-        {' '}
-        Detail-oriented Full Stack Software Engineer with a passion for frontend
-        design and the user experience. Motivated self-starter excited about
-        creating responsive and efficient web-based experiences. Skilled in
-        creative problem solving while collaborating and communicating with
-        compassion and empathy. A strong believer in the importance of learning
-        and forever having a growth mindset. Patient problem solver with the
-        ability to multi-task and deliver in high pressure, chaotic
-        environments. A professional background in artist management, graphic
-        design and social media marketing, developing grit, flexibility and a
-        dedicated work ethic.{' '}
-      </h4>
+    <>
+      <h1 className="about-header-two text-center container">
+        More About Me :
+      </h1>{' '}
+      <div className="about-two-container container">
+        <div className="about-image-container">
+          <img className="about-two-image" src={me} alt="" />
+        </div>
+        <div className="about-text-two">
+          <h4>
+            Detail-oriented Full Stack Software Engineer with a passion for
+            frontend design and the user experience. Motivated self-starter
+            excited about creating responsive and efficient web-based
+            experiences. Skilled in creative problem solving while collaborating
+            and communicating with compassion and empathy. A strong believer in
+            the importance of learning and forever having a growth mindset.
+            Patient problem solver with the ability to multi-task and deliver in
+            high pressure, chaotic environments. A professional background in
+            artist management, graphic design and social media marketing,
+            developing grit, flexibility and a dedicated work ethic.
+          </h4>{' '}
+        </div>
+      </div>
       <div className="about-section">
         <div className="section-divider">
-          <div className="frontend-about">
-            <h2 className="front-title">Frontend Development </h2>
-            <ul className="front-end-list">
-              <li className="skills-items">HTML</li>
-              <li className="skills-items">CSS</li>
-              <li className="skills-items">JavaScript</li>
-              <li className="skills-items">React</li>
-              <li className="skills-items">UX/UI </li>
-              <li className="skills-items">Graphic Design</li>
+          <div className="frontend-two">
+            <h2 className="front-title-two">Frontend Development </h2>
+            <ul className="front-end-list-two">
+              <li className="skills-items-two">HTML</li>
+              <li className="skills-items-two">CSS</li>
+              <li className="skills-items-two">JavaScript</li>
+              <li className="skills-items-two">React</li>
+              <li className="skills-items-two">UX/UI </li>
+              <li className="skills-items-two">Graphic Design</li>
             </ul>
           </div>
-          <div className="backend">
-            <h2 className="back-title">Backend Development </h2>
-            <ul className="back-end-list">
-              <li className="skills-items">Node JS</li>
-              <li className="skills-items">Express</li>
-              <li className="skills-items">MongoDB</li>
-              <li className="skills-items">MySQL</li>
-              <li className="skills-items">Python</li>
-              <li className="skills-items">Django</li>
+          <div className="backend-two">
+            <h2 className="back-title-two">Backend Development </h2>
+            <ul className="back-end-list-two">
+              <li className="skills-items-two">Node JS</li>
+              <li className="skills-items-two">Express</li>
+              <li className="skills-items-two">MongoDB</li>
+              <li className="skills-items-two">MySQL</li>
+              <li className="skills-items-two">Python</li>
+              <li className="skills-items-two">Django</li>
             </ul>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
