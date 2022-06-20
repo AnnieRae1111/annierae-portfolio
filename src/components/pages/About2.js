48814dd3b3102b712511172e01129628f43bced1
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-06-20 13:16:08 -0600

    about two page for now

--- src/components/pages/About2.js
+++ src/components/pages/About2.js
@@ -12,18 +12,21 @@ const AboutTwo = () => {
           <img className="about-two-image" src={me} alt="" />
         </div>
         <div className="about-text-two">
-          <h4>
-            Detail-oriented Full Stack Software Engineer with a passion for
-            frontend design and the user experience. Motivated self-starter
-            excited about creating responsive and efficient web-based
-            experiences. Skilled in creative problem solving while collaborating
-            and communicating with compassion and empathy. A strong believer in
-            the importance of learning and forever having a growth mindset.
-            Patient problem solver with the ability to multi-task and deliver in
-            high pressure, chaotic environments. A professional background in
-            artist management, graphic design and social media marketing,
-            developing grit, flexibility and a dedicated work ethic.
-          </h4>{' '}
+          <div className="text-container">
+            <h4 className="about-two-h4">
+              Detail-oriented Full Stack Software Engineer with a passion for
+              frontend design and the user experience. Motivated self-starter
+              excited about creating responsive and efficient web-based
+              experiences. Skilled in creative problem solving while
+              collaborating and communicating with compassion and empathy. A
+              strong believer in the importance of learning and forever having a
+              growth mindset. Patient problem solver with the ability to
+              multi-task and deliver in high pressure, chaotic environments. A
+              professional background in artist management, graphic design and
+              social media marketing, developing grit, flexibility and a
+              dedicated work ethic.
+            </h4>{' '}
+          </div>
         </div>
       </div>
       <div className="about-section">
