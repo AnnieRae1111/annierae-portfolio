ae29e1ba7876c765c0a46c32e00714823130839d
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-07-29 10:06:25 -0600

    added other relevant skills to more about me section:

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -21,25 +21,36 @@ const About = () => {
           <div className="frontend">
             <h2 className="front-title">Frontend Development </h2>
             <ul className="front-end-list">
-              <li className="skills-items-two">HTML</li>
-              <li className="skills-items-two">CSS</li>
-              <li className="skills-items-two">Bootstrap</li>
-              <li className="skills-items-two">Tailwind CSS</li>
-              <li className="skills-items-two">JavaScript</li>
-              <li className="skills-items-two">React</li>
-              <li className="skills-items-two">UX/UI </li>
-              <li className="skills-items-two">Graphic Design</li>
+              <li className="skills-items-two">• HTML</li>
+              <li className="skills-items-two">• CSS</li>
+              <li className="skills-items-two">• Bootstrap</li>
+              <li className="skills-items-two">• Tailwind CSS</li>
+              <li className="skills-items-two">• JavaScript</li>
+              <li className="skills-items-two">• React</li>
+              {/* <li className="skills-items-two">• UX/UI </li>
+              <li className="skills-items-two"> • Graphic Design</li> */}
             </ul>
           </div>
           <div className="backend">
             <h2 className="back-title">Backend Development </h2>
             <ul className="back-end-list">
-              <li className="skills-items">Node JS</li>
-              <li className="skills-items">Express</li>
-              <li className="skills-items">MongoDB</li>
-              <li className="skills-items">MySQL</li>
-              <li className="skills-items">Python</li>
-              <li className="skills-items">Django</li>
+              <li className="skills-items">• Node JS</li>
+              <li className="skills-items">• Express</li>
+              <li className="skills-items">• MongoDB</li>
+              <li className="skills-items">• MySQL</li>
+              <li className="skills-items">• Python</li>
+              <li className="skills-items">• Django</li>
+            </ul>
+          </div>
+          <div className="backend">
+            <h2 className="back-title">Other Relevant Skills </h2>
+            <ul className="back-end-list">
+              <li className="skills-items">• Brand Management</li>
+
+              <li className="skills-items">• E-Commerce</li>
+              <li className="skills-items">• Social Media Marketing</li>
+              <li className="skills-items">• UX/UI</li>
+              <li className="skills-items">• Graphic Design</li>
             </ul>
           </div>
         </div>
